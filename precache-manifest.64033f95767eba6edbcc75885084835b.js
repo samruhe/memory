fd1743c12bf1df79947834767dbf01aa71b6be1c
Sam Ruhe <sar420@lehigh.edu>
2019-07-30 16:24:58 +0200

    Updates

---
+++ precache-manifest.64033f95767eba6edbcc75885084835b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2715b960bb66dfca85deb48830c26286",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "3c966c9e8c20f48224b4",
+    "url": "/myapp/static/css/main.8914d126.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/myapp/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "3c966c9e8c20f48224b4",
+    "url": "/myapp/static/js/main.a55a4152.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
